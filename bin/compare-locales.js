d023aa2d2891ebdd53a39f31e35ba66143358bcb
Zbigniew Braniecki <gandalf@mozilla.com>
2014-07-17 16:23:57 -0700

    compareDictionaries

--- bin/compare-locales.js
+++ bin/compare-locales.js
@@ -5,20 +5,23 @@
 var program = require('commander');
 
 var GaiaApp = require('../lib/mozilla/apps/gaia.js').GaiaApp;
-var L10nCluster = require('../lib/mozilla/l10ncluster.js').L10nCluster;
-var compareApp = require('../lib/mozilla/compare-locales.js').compareApp;
+var Directory = require('../lib/mozilla/langpack.js').Directory;
+var compareDirs = require('../lib/mozilla/compare-locales.js').compareDirectories;
 
 var l10nBase = '~/projects/gaia-l10n';
 var locales = ['pl'];
-var path = '/Users/zbraniecki/projects/gaia/apps/settings';
+
+var path1 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/en-US/apps/settings';
+var path2 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/pl/apps/settings';
 
 function compareLocales() {
-  var app = new GaiaApp(path);
+  var dir1 = new Directory(path1);
 
-  var l10nCluster = new L10nCluster();
+  var dir2 = new Directory(path2);
 
-  compareApp(app, l10nCluster, locales);
+  compareDirs(dir1, dir2);
 }
+
 
 program
   .version('0.0.1')
