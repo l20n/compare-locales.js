e8a07b93ac0da98e5f81c3f56cbf1c2b4de102b2
Zbigniew Braniecki <gandalf@mozilla.com>
2014-09-09 01:05:49 -0700

    updates

--- bin/compare-locales.js
+++ bin/compare-locales.js
@@ -6,20 +6,22 @@ var program = require('commander');
 
 var GaiaApp = require('../lib/mozilla/apps/gaia.js').GaiaApp;
 var Directory = require('../lib/mozilla/langpack.js').Directory;
+var Langpack = require('../lib/mozilla/langpack.js').Langpack;
 var compareDirs = require('../lib/mozilla/compare-locales.js').compareDirectories;
 
 var l10nBase = '~/projects/gaia-l10n';
 var locales = ['pl'];
 
-var path1 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/en-US/apps/settings';
-var path2 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/pl/apps/settings';
+//var path1 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/en-US/apps/settings';
+//var path2 = '/Users/zbraniecki/projects/gaia-l10n/v1_3/pl/apps/settings';
+var path1 = '/Users/zbraniecki/projects/gaia/apps/wappush';
 
 function compareLocales() {
-  var dir1 = new Directory(path1);
-
-  var dir2 = new Directory(path2);
-
-  compareDirs(dir1, dir2);
+  //var dir1 = new Directory(path1);
+  //var dir2 = new Directory(path2);
+  //compareDirs(dir1, dir2);
+  var app = new GaiaApp(path1);
+  app.getLangpacks();
 }
 
 
