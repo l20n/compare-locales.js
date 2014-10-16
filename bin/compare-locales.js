c1f0b802aef31fafdcfd915fd2099413b123d775
Zibi Braniecki <gandalf@mozilla.com>
2014-10-16 12:55:24 +0200

    Support extrnal langpack directories

--- bin/compare-locales.js
+++ bin/compare-locales.js
@@ -10,24 +10,44 @@ var compareLangpacks =
 var serializeLangpackDiffToText =
   require('../lib/mozilla/diff/serialize.js').serializeLangpackDiffToText;
 
-var langPath = '/Users/zbraniecki/projects/gaia-l10n/master/fr';
-
 function compareLocales(appPath, lang) {
   var app = new GaiaApp(appPath);
-  app.getLangpacks().then(function() {
-    var lp1 = app.langpacks[app.defaultLocale];
-    var lp2 = app.langpacks[lang];
-    var lpDiff = compareLangpacks(lp1, lp2);
-    var txt = serializeLangpackDiffToText(lpDiff);
-    console.log(txt);
-  });
 
+  app.collectResources().then(function() {
+    app.getLangpacks().then(function() {
+      var lp1 = app.langpacks[app.defaultLocale];
+      var lp2 = app.langpacks[lang];
+      var lpDiff = compareLangpacks(lp1, lp2);
+      var txt = serializeLangpackDiffToText(lpDiff);
+      console.log(txt);
+    });
+  });
 }
 
+function compareLocales2(appPath, langPath, lang) {
+  var app = new GaiaApp(appPath);
+
+  app.collectResources().then(function() {
+
+    var lp = app.getLangpackFromPath(langPath);
+
+    app.getLangpacks().then(function() {
+      var lp1 = app.langpacks[app.defaultLocale];
+      var lp2 = app.langpacks[lang];
+      var lpDiff = compareLangpacks(lp1, lp);
+      var txt = serializeLangpackDiffToText(lpDiff);
+      console.log(txt);
+    });
+  });
+}
 
 program
   .version('0.0.1')
   .usage('[options] webapp locale')
   .parse(process.argv);
 
-compareLocales(program.args[0], program.args[1]);
+if (program.args.length === 3) {
+  compareLocales2(program.args[0], program.args[1], program.args[2]);
+} else {
+  compareLocales(program.args[0], program.args[1]);
+}
