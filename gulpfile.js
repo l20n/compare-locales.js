e3e0f5e8800ace140590ea964f79128c47e20419
Staś Małolepszy <stas@mozilla.com>
2014-10-17 13:15:46 +0200

    Add gulp default and gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,21 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
+var sourceGlob = './{bin,lib,tests}/**/*.js';
+
+gulp.task('default', ['lint', 'test']);
+
+gulp.task('lint', function() {
+    return gulp.src(sourceGlob)
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
+});
+
 gulp.task('test', function () {
   return gulp.src('tests/*.js', { read: false })
     .pipe(mocha({ ui: 'tdd' }));
 });
 
-gulp.task('lint', function() {
-    return gulp.src('./{bin,lib,tests}/**/*.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish));
+gulp.task('watch', function() {
+    return gulp.watch(sourceGlob, ['test']);
 });
