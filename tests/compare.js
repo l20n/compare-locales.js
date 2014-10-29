6fae23774f3ee193d346128513d8657f96be147b
Staś Małolepszy <stas@mozilla.com>
2014-10-29 18:11:47 +0100

    tests for all 4 modes

--- tests/compare.js
+++ tests/compare.js
@@ -10,30 +10,58 @@ var cl = require('../lib/mozilla/compare-locales.js');
 var serializeLangpackDiffToText =
   require('../lib/mozilla/diff/serialize.js').serializeLangpackDiffToText;
 
-function checkOutput(done, fixture, output) {
-  fs.readFile(fixture, {encoding: 'utf8'}, function(err, data) {
-    if (err) {
-      throw err;
-    }
+function getOutputPath(name) {
+  return path.join(__dirname, 'fixtures', 'output', name + '.txt');
+}
 
-    assert.equal(output, data.slice(0, -1));
-    done();
-  });
+function checkOutput(done, fixtureName, output) {
+  fs.readFile(getOutputPath(fixtureName), {encoding: 'utf8'},
+    function(err, data) {
+      if (err) {
+        throw err;
+      }
+
+      assert.equal(output, data.slice(0, -1));
+      done();
+    });
 }
 
 function logError(e) {
   console.error(e.stack);
 }
 
-suite('Compare langpacks in source', function() {
+suite('Comparison modes', function() {
+  var appPath = path.join(__dirname, 'fixtures', 'apps', 'clock');
+  var l10nPath = path.join(__dirname, 'fixtures', 'locales');
+  var enUSPath = path.join(__dirname, 'fixtures', 'locales', 'en-US');
+  var frPath = path.join(__dirname, 'fixtures', 'locales', 'fr');
 
-  test('compares two langpacks', function(done) {
-    var appPath = path.join(__dirname, 'fixtures', 'apps', 'clock');
-    var outputPath = path.join(__dirname, 'fixtures', 'output.txt');
-
+  test('compare langpacks in source', function(done) {
     cl.compareLangpacksInSource(appPath, null, 'fr').then(
       serializeLangpackDiffToText).then(
-        checkOutput.bind(null, done, outputPath)).catch(logError);
+        checkOutput.bind(null, done, 'compareLangpacksInSource'))
+          .catch(logError);
+  });
+
+  test('compare l10n dir to source', function(done) {
+    cl.compareL10nDirToSource(appPath, null, l10nPath, 'fr').then(
+      serializeLangpackDiffToText).then(
+        checkOutput.bind(null, done, 'compareL10nDirToSource'))
+          .catch(logError);
+  });
+
+  test('compare l10n tree dirs', function(done) {
+    cl.compareL10nTreeDirs(l10nPath, null, 'fr').then(
+      serializeLangpackDiffToText).then(
+        checkOutput.bind(null, done, 'compareL10nTreeDirs'))
+          .catch(logError);
+  });
+
+  test('compare dirs', function(done) {
+    cl.compareDirs(enUSPath, frPath).then(
+      serializeLangpackDiffToText).then(
+        checkOutput.bind(null, done, 'compareDirs'))
+          .catch(logError);
   });
 
 });
