0fddeafbbf7c3d7b51e71e304225c75a1fd29ea2
Zibi Braniecki <gandalf@mozilla.com>
2015-05-20 11:49:04 -0700

    Replace commander lib with custom commander

--- bin/compare-dirs.js
+++ bin/compare-dirs.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var program = require('commander');
+var program = require('../lib/commander');
 
 var cl = require('../lib/mozilla/compare-locales');
 
@@ -24,10 +24,10 @@ program
   .version('0.0.1')
   .usage('[options] reference locale')
   .option('-t, --type <gaia|gecko>', 'App type (default: gaia)', 'gaia')
-  .option('--data <text|json|exhibit>', 'Output type (default: text)',
-    'compat_text')
+  .option('--data <text|json|exhibit>', 'Output type (default: text)', 'text')
   .option('-r, --run-tests <tests>', 'Run tests', '')
   .parse(process.argv);
+
 
 var dirs = program.args;
 
